5e302d39befcabba60c7f920245c1311b78570d5
Dan Horrigan <dan@dhorrigan.com>
2011-03-03 22:41:09 -0500

    Removed accidental test copy-paste

--- test/install_test.php
+++ test/install_test.php
@@ -54,41 +54,4 @@ class Install_Test extends Spark_Test_Case {
         ob_end_clean();
     }
 
-    function test_remove() {
-        ob_start();
-        // Test install with a version specified
-        $this->cli->execute('install', array('-v1.0', 'example-spark')); // Spark needs installed first
-        $clines = $this->capture_buffer_lines(function($cli) {
-            $cli->execute('remove', array('-v1.0', 'example-spark'));
-        });
-        $success = (bool) (strpos(end($clines), '[ SPARK ]  Spark removed') === 0 && ! is_dir(SPARK_PATH.'/example-spark'));
-        $this->assertEquals(true, $success);
-
-        // Test install without a version or -f specified
-        $this->cli->execute('install', array('-v1.0', 'example-spark')); // Spark needs installed first
-        $clines = $this->capture_buffer_lines(function($cli) {
-            $cli->execute('remove', array('example-spark'));
-        });
-        $success = (bool) (strpos(end($clines), '[ ERROR ]  Please specify') === 0 && is_dir(SPARK_PATH.'/example-spark'));
-        $this->assertEquals(true, $success);
-
-        // Test install with a -f specified
-        $this->cli->execute('install', array('-v1.0', 'example-spark')); // Spark needs installed first
-        $clines = $this->capture_buffer_lines(function($cli) {
-            $cli->execute('remove', array('-f', 'example-spark'));
-        });
-        $success = (bool) (strpos(end($clines), '[ SPARK ]  Spark removed') === 0 && ! is_dir(SPARK_PATH.'/example-spark'));
-        $this->assertEquals(true, $success);
-
-        // Test install with a wrong version specified
-        $this->cli->execute('install', array('-v1.0', 'example-spark')); // Spark needs installed first
-        $clines = $this->capture_buffer_lines(function($cli) {
-            $cli->execute('remove', array('-v33.4', 'example-spark'));
-        });
-        $success = (bool) (strpos(end($clines), '[ SPARK ]  Looks like that spark isn\'t installed') === 0 && is_dir(SPARK_PATH.'/example-spark'));
-        $this->assertEquals(true, $success);
-        ob_end_clean();
-    }
-
-
 }
